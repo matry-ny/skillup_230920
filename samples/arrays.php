45f08d57b8e314a66721bf0c72e0acf719b6cd2c
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2020-10-07 21:17:48 +0300

    L05: Added array functions example

--- samples/arrays.php
+++ samples/arrays.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(E_ALL);
+
 $a1 = array('test', 'qwerty', 123);
 $a2 = [4 => 'test', 2 => 'qwerty', 123];
 $a3 = [
@@ -13,11 +15,11 @@ $groups = [
         'id' => 1,
         'name' => 'PHP Basics',
         'students' => [
-            [
+            's1' => [
                 'name' => 'Med Max',
                 'age' => 33,
             ],
-            [
+            's2' => [
                 'name' => 'Ned Flanders',
                 'age' => 52,
             ],
@@ -57,3 +59,47 @@ array_push($groups[0]['students'], [
 ]);
 
 //var_dump($groups);
+
+$string = 'Hello World';
+//var_dump($string[0], $string[-1]);
+
+$a4 = [
+    'qwerty',
+    10,
+    4,
+    1,
+    6,
+    10 => 'test',
+    15 => 'test',
+];
+
+rsort($a4);
+
+//var_dump($a4);
+
+$students = $groups[0]['students'];
+
+usort($students, static function (array $a, array $b) : int {
+    return $b['age'] <=> $a['age'];
+});
+
+//var_dump(count($groups));
+
+$a4Start = array_shift($a4);
+$a4_2 = $a4[2];
+unset($a4[2]);
+//var_dump(
+//    $a4Start,
+//    $a4_2,
+//    $a4,
+//    array_key_exists(2, $a4),
+//    in_array('qwerty', $a4),
+////    array_unique($a4)
+//    array_flip(array_flip($a4)),
+//    compact('a4Start', 'a4_2', 'students')
+//);
+
+$GLOBALS['a4_2'] = 123123123132;
+
+//session_start();
+//var_dump($_SESSION['test']);
