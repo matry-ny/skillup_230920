56155d7e3444085b586445a8b0e5cd4eeef89602
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2020-10-15 20:09:23 +0300

    L07: Added global vars inside functions demonstration

--- samples/functions.php
+++ samples/functions.php
@@ -35,11 +35,81 @@ $func1 = static function (int $a1 = 0) : void {
 };
 
 $printFunc = 'printName';
-$printFunc('Dmytro Kotenko');
-printName('Homer Simpson', 'Donut');
-printName(null, 'Shiny Ass', $func1);
+//$printFunc('Dmytro Kotenko');
+//printName('Homer Simpson', 'Donut');
+//printName(null, 'Shiny Ass', $func1);
 //printName(123);
 
-var_dump($q);
+//var_dump($q);
 
-$func1(time());
+//$func1(time());
+
+$global1 = '123';
+
+function globalFunction($global1) : void
+{
+    $functionVar = 3333;
+    // CASE 1. Don't even mind
+    /*
+    var_dump('inside >>> ' . $GLOBALS['global1']);
+    $GLOBALS['global1'] = mt_rand();
+    unset($GLOBALS['global1']);
+    */
+
+    // CASE 2. Just mind. not realize
+    /*
+    global $global1;
+    var_dump('inside >>> ' . $global1);
+    $global1 = mt_rand();
+    unset($global1);
+    */
+
+    // CASE 3. OK
+    var_dump('inside >>> ' . $global1);
+    $global1 = mt_rand();
+    unset($global1);
+}
+
+//globalFunction($global1);
+//var_dump('outside >>> ' . $global1);
+//
+//globalFunction($global1);
+//var_dump('outside 2 >>> ' . $global1);
+
+$globalFunction2 = static function () use ($global1) : int {
+    var_dump('inside >>> ' . $global1);
+    $global1 = mt_rand();
+    unset($global1);
+
+    return 1;
+};
+
+//$globalFunction2();
+//var_dump('outside >>> ' . $global1);
+//
+//$globalFunction2();
+//var_dump('outside 2 >>> ' . $global1);
+
+function linkedFunc(&$link) : void
+{
+    var_dump('inside >>> ' . $link);
+    $link = mt_rand();
+    var_dump('inside 2 >>> ' . $link);
+    unset($link);
+    $link = mt_rand();
+    var_dump('inside 3 >>> ' . $link);
+}
+
+//linkedFunc($global1);
+//var_dump('outside >>> ' . $global1);
+
+function returnFunction(string $arg) : string
+{
+    var_dump('inside >>> ' . $arg);
+    $arg = (string)mt_rand();
+
+    return $arg;
+}
+
+$global1 = returnFunction($global1);
+var_dump('outside >>> ' . $global1);
