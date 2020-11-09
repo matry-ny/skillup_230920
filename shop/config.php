b59e0f130d0817a9d6d1b2ce0b6e5c3f0848b692
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2020-11-09 21:44:33 +0200

    L14: Added shop database structure

---
+++ shop/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'baseDir' => __DIR__,
+    'webRout' => '/shop',
+    'db' => [
+        'host' => 'tiba00.mysql.tools',
+        'user' => 'tiba00_skillupshop',
+        'password' => 'vs9-&9T4Ne',
+        'db' => 'tiba00_skillupshop',
+    ],
+];
