8657ef0a26d559a273213af740f881211103ea4c
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2020-11-18 21:31:37 +0200

    L17: Added more OOP features demonstration

--- oop/classes/index.php
+++ oop/classes/index.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/Human.php';
 require_once __DIR__ . '/Student.php';
 require_once __DIR__ . '/Mentor.php';
 
-var_dump(Student::BIOLOGIC);
+//var_dump(Student::BIOLOGIC);
 
 $student = new Student();
 $student->setName('Mark Antonio');
@@ -23,18 +23,32 @@ $mentor = new Mentor();
 $mentor->setName('Phillip Kirkorov');
 $mentor->setAge(58);
 
-echo "{$student->getName()}: {$student->makeHomework()}", PHP_EOL;
-echo "{$student2->getName()}: {$student2->makeHomework()}", PHP_EOL;
-echo "{$mentor->getName()}: {$mentor->checkHomework()}", PHP_EOL;
+//echo "{$student->getName()}: {$student->makeHomework()}", PHP_EOL;
+//echo "{$student2->getName()}: {$student2->makeHomework()}", PHP_EOL;
+//echo "{$mentor->getName()}: {$mentor->checkHomework()}", PHP_EOL;
 
 $student2->breathe('O2');
 $student2->breathe('O2');
 $student2->breathe('O2');
 $student2->breathe('O2');
 
-var_dump($student, $student2, $mentor);
+//var_dump($student, $student2, $mentor);
 
-var_dump($student->writeCode(), $mentor->writeCode());
+//var_dump($student->writeCode(), $mentor->writeCode());
+
+changeMentor($mentor);
+
+function changeMentor(Mentor $mentor)
+{
+    $mentor->setName('Inside Function');
+}
+
+$mentor2 = clone $mentor;
+$mentor2->setName('Mentor Two');
+$mentor->setName('Mentor One');
+//var_dump($mentor, $mentor2);
+
+var_dump($student instanceof Human, $mentor instanceof Human);
 
 // Homework
 // создать классы Homework, Mentor, Student
