3ae9c63de9e17109a1b20b6047fb55d22f157e97
Dmytro Kotenko <dmytro.kotenko@mgid.com>
2020-11-23 20:35:19 +0200

    L18: Added abstract classes and interfaces

---
+++ oop/abstractions/Cat.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Class Cat
+ */
+class Cat extends Animal implements ScratchableInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function makeSound(): string
+    {
+        return 'Myau';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function scratch(): string
+    {
+        return "{$this->getClawsCount()} Scratches and {$this->makeSound()}";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getClawsCount(): int
+    {
+        return 5;
+    }
+}
